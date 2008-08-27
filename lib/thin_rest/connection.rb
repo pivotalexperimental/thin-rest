7c7365a7276ce38db300713bd1e96258b411a1bb
Brian Takita <brian.takita@gmail.com>
2008-08-26 19:57:25 -0700

    Connection#send_head takes additional parameters.

--- lib/thin_rest/connection.rb
+++ lib/thin_rest/connection.rb
@@ -14,12 +14,14 @@ module ThinRest
       @rack_request ||= Rack::Request.new(@request.env)
     end
 
-    def send_head(status=200)
-      send_data(head(status))
+    def send_head(status=200, additional_parameters={})
+      send_data(head(status, additional_parameters))
     end
 
-    def head(status)
-      "HTTP/1.1 #{status} OK\r\nConnection: close\r\nServer: Thin Rest Server\r\n"
+    def head(status, additional_parameters)
+      additional_parameters.inject("HTTP/1.1 #{status} OK\r\nConnection: close\r\nServer: Thin Rest Server\r\n") do |header, parameters|
+        header << "#{parameters[0]}: #{parameters[1]}\r\n"
+      end
     end
 
     def send_body(data)
