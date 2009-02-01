d0c5d0fc631bcc59fe04f1663934a2f9b33cadf0
Brian Takita <btakita@btakita-laptop.(none)>
2009-01-31 19:25:06 -0500

    Removing ResourceInvalid because it is not used.

--- lib/thin_rest.rb
+++ lib/thin_rest.rb
@@ -6,6 +6,5 @@ dir = File.dirname(__FILE__)
 require "#{dir}/thin_rest/connection"
 require "#{dir}/thin_rest/resources"
 require "#{dir}/thin_rest/representations"
-require "#{dir}/thin_rest/resource_invalid"
 require "#{dir}/thin_rest/routing_error"
 require "#{dir}/thin_rest/extensions"
